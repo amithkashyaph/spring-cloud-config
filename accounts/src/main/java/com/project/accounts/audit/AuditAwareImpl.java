7755841d1b74153166ef938d29cade78d95c9f4c
amithkashyap <amithkashyaprv@gmail.com>
2024-06-08 01:20:06 +0530

    Added audit aware class for accounts service

---
+++ accounts/src/main/java/com/project/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,13 @@
+package com.project.accounts.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNTS_MS");
+    }
+}
