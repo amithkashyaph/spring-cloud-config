f8021651a90f086a42610a18fd3d296e213b485a
amithkashyap <amithkashyaprv@gmail.com>
2024-09-29 22:17:03 +0530

    added initial account service impl

---
+++ accounts/src/main/java/com/project/accounts/service/impl/AccountServiceImpl.java
@@ -0,0 +1,25 @@
+package com.project.accounts.service.impl;
+
+import com.project.accounts.constants.AccountConstants;
+import com.project.accounts.dto.AccountDto;
+import com.project.accounts.dto.CustomerDto;
+import com.project.accounts.entity.Account;
+import com.project.accounts.entity.Customer;
+import com.project.accounts.exception.CustomerAlreadyExistsException;
+import com.project.accounts.exception.ResourceNotFoundException;
+import com.project.accounts.mapper.AccountMapper;
+import com.project.accounts.mapper.CustomerMapper;
+import com.project.accounts.repository.AccountRepository;
+import com.project.accounts.repository.CustomerRepository;
+import com.project.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl {
+}
