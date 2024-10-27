6a79206c105f682eab7dd57b0a1ea9ade0500e9e
amithkashyap <amithkashyaprv@gmail.com>
2024-10-27 23:48:50 +0530

    added initial customer entity

---
+++ accounts/src/main/java/com/project/accounts/entity/Customer.java
@@ -0,0 +1,13 @@
+package com.project.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class Customer extends BaseEntity {
+
+
+}
