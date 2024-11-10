54aba40496dd1364a5dbb42ef439bf3d1b640ca0
amithkashyap <amithkashyaprv@gmail.com>
2024-11-11 00:06:42 +0530

    updated loan repository

--- loans/src/main/java/com/project/loans/repository/LoansRepository.java
+++ loans/src/main/java/com/project/loans/repository/LoansRepository.java
@@ -10,4 +10,8 @@ import java.util.Optional;
 @Repository
 public interface LoansRepository extends JpaRepository<Loans, Long> {
 
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+
 }
