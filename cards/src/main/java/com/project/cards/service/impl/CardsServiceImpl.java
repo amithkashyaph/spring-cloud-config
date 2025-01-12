76b2d8ed04e15c4b3aba7609d6e5ed997dd13be8
amithkashyap <amithkashyaprv@gmail.com>
2025-01-12 20:30:45 +0530

    added initial card service class

---
+++ cards/src/main/java/com/project/cards/service/impl/CardsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.project.cards.service.impl;
+
+
+import com.project.cards.constants.CardsConstants;
+import com.project.cards.dto.CardsDto;
+import com.project.cards.entity.Cards;
+import com.project.cards.exception.CardAlreadyExistsException;
+import com.project.cards.exception.ResourceNotFoundException;
+import com.project.cards.mapper.CardsMapper;
+import com.project.cards.repository.CardsRepository;
+import com.project.cards.service.CardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class CardsServiceImpl {
+
+
+}
