bfb81714de19c5567ab3c1ca6d15434fc74cad10
amithkashyap <amithkashyaprv@gmail.com>
2025-02-19 23:45:14 +0530

    added create new card service logic

--- cards/src/main/java/com/project/cards/service/impl/CardsServiceImpl.java
+++ cards/src/main/java/com/project/cards/service/impl/CardsServiceImpl.java
@@ -33,4 +33,20 @@ public class CardsServiceImpl implements CardsService {
         cardsRepository.save(createNewCard(mobileNumber));
     }
 
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new card details
+     */
+    private Cards createNewCard(String mobileNumber) {
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
+    }
+
 }
