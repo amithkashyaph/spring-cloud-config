493961173d4ff02b9ca1e09f44fcd92da9679da4
amithkashyap <amithkashyaprv@gmail.com>
2025-02-26 22:02:55 +0530

    added resource not found exception handler method

--- loans/src/main/java/com/project/loans/exception/GlobalExceptionHandler.java
+++ loans/src/main/java/com/project/loans/exception/GlobalExceptionHandler.java
@@ -47,4 +47,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                            WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
+
 }
