f6d3a9306eaee8e0c0ff06edc6eadae51a1569d1
amithkashyap <amithkashyaprv@gmail.com>
2025-06-28 13:28:11 +0530

    added create new loan service logic

--- loans/src/main/java/com/project/loans/service/impl/LoansServiceImpl.java
+++ loans/src/main/java/com/project/loans/service/impl/LoansServiceImpl.java
@@ -32,4 +32,20 @@ public class LoansServiceImpl implements LoansService {
         loansRepository.save(createNewLoan(mobileNumber));
     }
 
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new loan details
+     */
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setMobileNumber(mobileNumber);
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
+
 }
